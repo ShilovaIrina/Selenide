e5c2de2e85bcb96167549bdcd9a3467b93da6362
Irina <irashilowa1997@mail.ru>
2024-07-14 21:11:16 +0300

    Add Test

---
+++ src/test/java/ru/netology/selenide/FormOrderACardTest.java
@@ -0,0 +1,35 @@
+package ru.netology.selenide;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class FormOrderACardTest {
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void formACardTest() {
+        Selenide.open("http://localhost:9999");
+        $(".input__control").setValue("Горно-Алтайск");
+        String plannerDate = generateDate(4, "dd.MM.yyyy");
+        // операция по очистке поля "Дата"
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(plannerDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79999999999");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $("[data-test-id='notification']")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + plannerDate));
+    }
+}
